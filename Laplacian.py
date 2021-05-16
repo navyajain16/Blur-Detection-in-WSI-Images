8bf75a06394cabbdea71809e71afd38b1b70beab
navyajain16 <72208843+navyajain16@users.noreply.github.com>
2021-05-16 15:51:38 +0530

    Update Laplacian.py

--- Laplacian.py
+++ Laplacian.py
@@ -13,7 +13,7 @@ def variance_of_laplacian(image):
 	# measure, which is simply the variance of the Laplacian
 	return cv2.Laplacian(image, cv2.CV_64F).var()
 
-images = "‪C:\blur detection\CLOUD" # path of images
+images = "‪C:\blur detection\CLOUD" # path of folder of images
 Threshold = 12
 
 for imagePath in paths.list_images(images):
